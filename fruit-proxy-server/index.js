927b9087a6f128ed829c07749bc560905029d665
itssidhere <sidjha0001@gmail.com>
2024-10-27 11:19:12 -0600

    fix cors issue in the api

--- fruit-proxy-server/index.js
+++ fruit-proxy-server/index.js
@@ -1,34 +1,26 @@
 const express = require('express');
-const axios = require('axios');  // Add this line
+const axios = require('axios');
+require('dotenv').config(); 
 
 const app = express();
 
-// CORS middleware
-app.use((req, res, next) => {
-    const allowedOrigins = ['https://fruit-proxy-server.vercel.app', 'http://localhost:5173'];
-    const origin = req.headers.origin;
-    
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
 
 app.get('/', async (req, res) => {
     try {
-        const response = await axios.get("https://wcz3qr33kmjvzotdqt65efniv40kokon.lambda-url.us-east-2.on.aws");
+        const response = await axios.get(process.env.TARGET_URL);
+        res.set('Access-Control-Allow-Origin', '*');
+        res.set('Access-Control-Allow-Headers', 'Content-Type');
+        res.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
         res.status(200).json(response.data);
+
     } catch (error) {
         console.error('Proxy error:', error.message);
         res.status(500).json({ error: 'Failed to fetch data' });
     }
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 3000}`);
 });
 
 module.exports = app;
